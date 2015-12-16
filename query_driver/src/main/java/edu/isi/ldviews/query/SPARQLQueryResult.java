8117e9589d825da95c94de6cbe07389cd36b900e
jasonslepicka <jasonslepicka@gmail.com>
2015-12-15 17:50:17 -0800

    Use CSV parser to handle sparql results

--- query_driver/src/main/java/edu/isi/ldviews/query/SPARQLQueryResult.java
+++ query_driver/src/main/java/edu/isi/ldviews/query/SPARQLQueryResult.java
@@ -1,18 +1,27 @@
 package edu.isi.ldviews.query;
 
+import java.io.IOException;
 import java.net.URLDecoder;
 import java.util.HashSet;
 import java.util.Random;
 import java.util.Set;
 
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
 public class SPARQLQueryResult implements QueryResult {
 
-	private String[] responses;
+	CSVParser parser = null;
 	public SPARQLQueryResult(String responseBody) {
-		this.responses = responseBody.split("\n");
+		try {
+			parser = CSVParser.parse(responseBody, CSVFormat.DEFAULT);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	public JSONObject getFacetValue(JSONObject queryType, Random rand) {
@@ -24,9 +33,15 @@ public class SPARQLQueryResult implements QueryResult {
 	public JSONArray getAnchorsFromResults(String field) {
 		JSONArray anchorsByResults = new JSONArray();
 		Set<String> uniqueURIs = new HashSet<String>();
-		for(int i = 1; i < responses.length; i++)
-		{
-			uniqueURIs.add((responses[i].substring(1,responses[i].indexOf(',')-1)));;
+		try {
+			for(CSVRecord record : parser.getRecords())
+			{
+				record.get(0);
+				uniqueURIs.add(record.get(0));
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 		for(String uri : uniqueURIs)
 		{
